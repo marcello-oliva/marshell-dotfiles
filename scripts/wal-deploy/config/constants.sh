df0fb9a9b3950f2c7ee3922c61be4d24712a98d8
marcello-oliva <olimars.dev@gmail.com>
2026-07-21 00:18:07 +0200

    feat(scripts/wal-deploy): add spicetify support

--- scripts/wal-deploy/config/constants.sh
+++ scripts/wal-deploy/config/constants.sh
@@ -6,14 +6,17 @@ readonly WAL_DEPLOY_CONFIG_LOADED=1
 # Display name of each application (used in log output and CLI lookups).
 readonly -a WAL_DEPLOY_APP_NAMES=(
     "dgop"
+    "spicetify"
 )
 
 # pywal-generated cache file
 readonly -a WAL_DEPLOY_CACHE_FILES=(
     "$HOME/.cache/wal/colors-dgop.json"
+    "$HOME/.cache/wal/colors-spicetify.ini"
 )
 
 # Destination symlink path
 readonly -a WAL_DEPLOY_TARGET_LINKS=(
     "$HOME/.config/dgop/colors.json"
+    "$HOME/.config/spicetify/Themes/pywal/color.ini"
 )
